38d90abadf52d415a5d3dadaad30d20799f60e09
myzel394 <miguel.kra@gmail.com>
2021-03-11 16:36:25 +0100

    default settings improved

--- settings.py
+++ settings.py
@@ -11,14 +11,15 @@ __all__ = [
 RESPOND_TO_GROUP_CHATS = False  # Respond to group chats
 # Respond to group chats privately - If False, responds in group chat to the person; If True, responds privately to
 # the person.
-RESPOND_TO_GROUP_CHATS_PRIVATE = True
+RESPOND_TO_GROUP_CHATS_PRIVATE = False
 # REPLY_TO_GROUP_WHEN_MENTIONED DOESN'T WORK!!! Please submit an idea or solution to this problem
 REPLY_TO_GROUP_WHEN_MENTIONED = False  # Only reply if mentioned in a message
 RESPOND_TO_UNKNOWN = False  # Respond to non-saved users
 
 RECOMMENDED_YOUTUBE_VIDEOS = [
+    "https://youtu.be/DKlUPmdxyp0",  # So Many Tabs
+    "https://youtu.be/hCut9HiQKl0",  # Algorithmen verstehen
     "https://youtu.be/rO37rz1dhkQ",  # The Morpheus Vlogs
-    "https://youtu.be/hCut9HiQKl0"  # Algorithmen verstehen
 ]
 
 # Your phone number (without spaces and prefixes, example: 4917612345678)
